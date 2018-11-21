e8a8463d27d82a2f6d681217a718c9af750866dc
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-11-21 23:09:05 +0100

    publish on twitter

--- src/main/java/com/graphaware/neo4j/discourse/rss/RssRoute.java
+++ src/main/java/com/graphaware/neo4j/discourse/rss/RssRoute.java
@@ -1,9 +1,12 @@
 package com.graphaware.neo4j.discourse.rss;
 
+import com.graphaware.neo4j.discourse.domain.ForumPost;
 import com.graphaware.neo4j.discourse.filter.AlreadyProcessedFilter;
 import com.graphaware.neo4j.discourse.filter.ForumPostFilter;
 import com.graphaware.neo4j.discourse.notification.SlackNotifier;
 import com.graphaware.neo4j.discourse.persistence.Neo4jPersister;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,6 +36,15 @@ public class RssRoute extends RouteBuilder {
                 .filter().method(alreadyProcessedFilter, "filter")
                 .filter().method(forumPostFilter, "filter")
                 .process(neo4jPersister)
-                .process(slackNotifier);
+                .process(slackNotifier)
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        ForumPost forumPost = exchange.getIn().getBody(ForumPost.class);
+                        String tweet = forumPost.getTitle() + " " + forumPost.getUrl() + "#neo4j";
+                        exchange.getOut().setBody(tweet);
+                    }
+                })
+                .to("twitter://timeline/user?consumerKey={{twitter.consumer.key}}&consumerSecret={{twitter.consumer.key.secret}}&accessToken={{twitter.access.token}}&accessTokenSecret={{twitter.access.token.secret}}");
     }
 }
